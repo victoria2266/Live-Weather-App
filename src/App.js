070ddd4327e3da693511553e002834385dac9873
Victoria <testversion2617@gmx.de>
2021-01-29 21:07:33 +0100

    commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Weathervideo from "./video/weathervideo.mp4"
+
+const api = {
+  key: "98e193595faf010315e7b901d60d1b5a",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [title, setTitle] = useState('Live Weather App');
+
+  console.log("wheater main:", weather.main);
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <h1>{title}</h1>
+
+      <video autoPlay loop muted
+        style={{
+          width: "100%",
+          minHeight: "100%"
+        }}>
+        <source src={(Weathervideo)} />
+      </video>
+
+      <div>
+        {(typeof weather.main != "undefined") ? (
+          <div className="weather-info">
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°C
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+
+            </div>
+          </div>
+        ) : ('')}
+      </div>
+
+
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        onChange={e => setQuery(e.target.value)}
+        onClick={() => setTitle('')}
+        value={query}
+        onKeyPress={search}
+      />
+
     </div>
   );
 }
